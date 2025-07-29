fbd3c12c0ae8071fe13980058de13baf32d98277
mari137 <mari.kovacs@outlook.com>
2025-07-29 23:20:12 +0200

    corrections

--- Core/UI/DriverFactory/ChromeFactory.cs
+++ Core/UI/DriverFactory/ChromeFactory.cs
@@ -17,7 +17,6 @@ internal class ChromeFactory : IDriverFactory
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
             options.AddArgument("disable-infobars");
-            options.AddArgument("--incognito");
             options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
         }
         else

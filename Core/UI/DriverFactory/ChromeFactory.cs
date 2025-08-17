4407bafe3abe5ff42208d23504a1e6e687d32a7c
mari137 <mari.kovacs@outlook.com>
2025-08-17 19:18:33 +0200

    trial11

--- Core/UI/DriverFactory/ChromeFactory.cs
+++ Core/UI/DriverFactory/ChromeFactory.cs
@@ -1,18 +1,11 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumUndetectedChromeDriver;
 
 namespace Core.UI.DriverFactory;
 
 internal class ChromeFactory : IDriverFactory
 {
-    private static readonly string[] UserAgents =
-    [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-     ];
-
     public IWebDriver CreateDriver(bool headless, string downloadDirectory)
     {
         var options = new ChromeOptions();
@@ -20,21 +13,9 @@ internal class ChromeFactory : IDriverFactory
 
         if (headless)
         {
-            options.AddArgument("--headless=new");
             options.AddArgument("--window-size=1920,1080");
-            options.AddArgument("--no-sandbox");
-            options.AddArgument("--disable-dev-shm-usage");
-            options.AddArgument("--auto-open-devtools-for-tabs");
-            options.AddArgument("disable-infobars");
-            options.AddArgument("--disable-blink-features=AutomationControlled");
-            options.AddExcludedArgument("enable-automation");
-            options.AddAdditionalOption("useAutomationExtension", false);
-            var agent = UserAgents[new Random().Next(UserAgents.Length)];
-            options.AddArgument($"--user-agent={agent}");
-            options.AddExcludedArguments(new List<string>() { "enable-automation" });
-
-            var driver = new ChromeDriver(options);
-            ((IJavaScriptExecutor)driver).ExecuteScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+            var driverPath = new ChromeDriverInstaller().Auto().Result;
+            var driver = UndetectedChromeDriver.Create(driverExecutablePath: driverPath, options: options, headless: true);
 
             return driver;
         }

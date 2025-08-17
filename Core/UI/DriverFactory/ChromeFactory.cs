b29fbc09a82f745079d0ca6a3c738c32c122a11f
mari137 <mari.kovacs@outlook.com>
2025-08-17 22:36:15 +0200

    trial14

--- Core/UI/DriverFactory/ChromeFactory.cs
+++ Core/UI/DriverFactory/ChromeFactory.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumUndetectedChromeDriver;
 
 namespace Core.UI.DriverFactory;
 
@@ -12,63 +13,9 @@ internal class ChromeFactory : IDriverFactory
 
         if (headless)
         {
-            options.AddArguments(new List<string>()
-            {
-                "--headless=new",
-                "--window-size=1920,1080",
-                "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                "--disable-blink-features=AutomationControlled",
-                "--disable-gpu",
-                "--no-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-web-security",
-                "--disable-features=VizDisplayCompositor",
-                "--disable-notifications",
-                "--disable-popup-blocking",
-                "--disable-infobars",
-                "--disable-logging",
-                "--disable-dev-tools",
-                "--no-first-run",
-                "--no-default-browser-check",
-                "--disable-background-timer-throttling",
-                "--disable-backgrounding-occluded-windows",
-                "--disable-renderer-backgrounding",
-                "--disable-features=TranslateUI",
-                "--disable-ipc-flooding-protection",
-                "--password-store=basic",
-                "--use-mock-keychain",
-                "--disable-client-side-phishing-detection",
-                "--disable-sync",
-                "--disable-features=UserAgentClientHint",
-                "--disable-blink-features",
-                "--disable-component-extensions-with-background-pages",
-            });
-
-            options.AddExcludedArgument("enable-automation");
-            options.AddAdditionalOption("useAutomationExtension", false);
-            options.AddUserProfilePreference("prefs", new Dictionary<string, object>
-                {
-                    ["credentials_enable_service"] = false,
-                    ["profile.password_manager_enabled"] = false,
-                });
-
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-            service.HideCommandPromptWindow = true;
-
-            IWebDriver driver = new ChromeDriver(service, options);
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-
-            js.ExecuteScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
-            js.ExecuteScript("window.chrome = { runtime: {} }");
-            js.ExecuteScript(@"
-                const originalQuery = window.navigator.permissions.query;
-                window.navigator.permissions.query = (parameters) => (
-                    parameters.name === 'notifications' ?
-                        Promise.resolve({ state: Notification.permission }) :
-                        originalQuery(parameters)
-                );
-            ");
-
+            options.AddArgument("--window-size=1920,1080");
+            var driverPath = new ChromeDriverInstaller().Auto().Result;
+            var driver = UndetectedChromeDriver.Create(driverExecutablePath: driverPath, options: options, headless: true);
             return driver;
         }
         else
